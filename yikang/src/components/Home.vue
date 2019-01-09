675bba4eb6fde0c1a59b617815adaf6509e2135e
weihuade <758785052@qq.com>
2019-01-09 11:29:36 +0800

    脚手架搭建好了

---
+++ yikang/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>333333333</h1>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+	color: red;
+	font-size:30px ;
+}
+</style>
